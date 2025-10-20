dbebd4d8f9ee45147a58145b3aa0e7001478a055
RamaHallia <rama@hallia.ai>
2025-10-20 16:41:54 +0200

    Updated Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -211,7 +211,7 @@ export function Dashboard() {
                       <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                     </svg>
                     <div>
-                      <span className="font-semibold text-gray-900">Dossier "TRAITÉS"</span>
+                      <span className="font-semibold text-gray-900">Dossier "TRAITE"</span>
                       <span className="text-gray-600"> - Emails traités</span>
                     </div>
                   </div>
